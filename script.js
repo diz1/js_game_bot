7b336e6eef4bea25a3de26def1f2da927ad107e1
diz1 <dima-smirnov1@mail.ru>
2020-04-02 13:48:07 +0300

    lesson_06_hard_done

--- script.js
+++ script.js
@@ -2,44 +2,68 @@
 const randomNumber = () => {
 	let number = 1;
 	return function () {
-		return number += Math.floor(Math.random() * 100);
+		return number = Math.floor(Math.random() * 100) || number;
 	}
 };
+const restart = () => {
+	number = getRandomNumber();
+	attempts = 10;
+	msg = prompt('Введите ваше число (Осталось попыток ' + attempts + ')');
+	validateNumber(msg);
+};
 const getRandomNumber = randomNumber();
-const number = getRandomNumber();
-alert('Загадано число от 1 до 100\r\nВаша задача его угадать.\r\nПоехали!');
-let msg = prompt('Введите ваше число');
-const validateNumber = (msg) => {
+let number = getRandomNumber();
+let attempts = 10;
+
+alert('Загадано число от 1 до 100\r\nВаша задача его угадать.\r\nУ вас ' + attempts + ' попыток' + '\r\nПоехали!');
+let msg = prompt('Введите ваше число (Осталось попыток ' + attempts + ')');
+
+const validateNumber = msg => {
 	let userNumber = msg;
 	if (typeof parseFloat(userNumber) === 'number' && !isNaN(userNumber) && typeof parseFloat(userNumber) !== 'object') {
-		alert('Сравниваем число с загаданным...');
-		if (parseFloat(userNumber) === number) {
-			alert('\u2705Молодец!\r\n\u2705Ты правильно угадал(-ла) число!\r\n\u2705Поздравляю!');
-			return;
-		} else if (userNumber === null) {
-			let conf = confirm('Похоже вы нажали «Отмена»\r\nЖелаете выйти?');
-			if (conf) {
+		if (attempts > 0) {
+			if (parseFloat(userNumber) === number) {
+				let conf = confirm('\u2705Молодец!\r\n\u2705Ты правильно угадал(-ла) число!' +
+					'\r\n\u2705Поздравляю!\r\nПопробовать еще раз?');
+				if (conf) {
+					restart();
+				}
 				return;
+			} else if (userNumber === null) {
+				let conf = confirm('Похоже вы нажали «Отмена»\r\nЖелаете выйти?');
+				if (!conf) {
+					msg = prompt('Введите ваше число (Осталось попыток ' + attempts + ')');
+					validateNumber(msg);
+				}
+				return;
+			} else if (parseFloat(userNumber) < number) {
+				--attempts;
+				if (attempts > 0) {
+					alert('\u274cНеверно!\r\n\u274cВаше число меньше загаданного\r\n\u274cПопробуй еще раз!');
+					msg = prompt('Введите ваше число (Осталось попыток ' + attempts + ')');
+					validateNumber(msg);
+				} else {
+					alert('Попытки закончились, попробуйте заного с новым числом');
+					restart();
+				}
+			} else if (parseFloat(userNumber) > number) {
+				--attempts;
+				if (attempts > 0) {
+					alert('\u274cНеверно!\r\n\u274cВаше число больше загаданного\r\n\u274cПопробуй еще раз!');
+					msg = prompt('Введите ваше число (Осталось попыток ' + attempts + ')');
+					validateNumber(msg);
+				} else {
+					alert('Попытки закончились, попробуйте заново с новым числом');
+					restart();
+				}
 			} else {
-				msg = prompt('Введите ваше число');
+				--attempts;
+				msg = prompt('Введено не число.\r\nВведите число! (Осталось попыток ' + attempts + ')');
 				validateNumber(msg);
 			}
-		} else if (parseFloat(userNumber) < number) {
-			alert('\u274cНеверно!\r\n\u274cВаше число меньше загаданного\r\n\u274cПопробуй еще раз!');
-			msg = prompt('Введите ваше число');
-			validateNumber(msg);
-		} else if (parseFloat(userNumber) > number) {
-			alert('\u274cНеверно!\r\n\u274cВаше число больше загаданного\r\n\u274cПопробуй еще раз!');
-			msg = prompt('Введите ваше число');
-			validateNumber(msg);
-		} else {
-			alert('\u274cНеверно!\r\n\u274cПопробуй еще раз!');
-			msg = prompt('Введите ваше число');
-			validateNumber(msg);
+		} else if (attempts === 0) {
+			restart();
 		}
-	} else {
-		msg = prompt('Введено не число.\r\nВведите число!');
-		validateNumber(msg);
 	}
 };
 
